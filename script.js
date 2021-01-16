86d19d26fc3579aa775afdba91f298f3d7151f62
Alex <am2invest@yandex.ru>
2021-01-16 10:33:37 +0500

    notes version 2.0 complete

--- script.js
+++ script.js
@@ -1,91 +1,94 @@
-const initialNotes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
-function render(notes){
-    const noteList = document.querySelector('.note-list');
-    const buttonSave = document.querySelector('.editor__save');
-    
-    noteList.innerHTML = null;
-    for(let idx = 0; idx < notes.length; idx++){
-        const listItem = renderNote(notes[idx].title, notes[idx].text);
-        listItem.onclick = () => {
-            openCloseEditor(notes[idx]);
-            buttonSave.onclick = () => saveNote(notes, idx);
-        };
-        noteList.append(listItem);
-    }
-}
+const notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
+let addButtonClose = false;
+let timeOut;
+
+const editorTitle = document.querySelector('.editor__title');
+const editorContent = document.querySelector('.editor__content');
 
 
-function renderNote(newTitle, newText){
-    const listItem = document.createElement('li');
-    listItem.classList.add('note');
-    const title = document.createElement('h2');
-    title.classList.add('note__title');
-    title.innerText = newTitle;
-    const text = document.createElement('p');
-    text.classList.add('note__preview');
-    text.innerText = newText;
-    listItem.append(title);
-    listItem.append(text);
-    return listItem;
-}
-/*
-const initialNotes = [
-    {
-        title: 'Пример заголовка 1',
-        text: 'Текст11111111111'
-    },
-    {
-        title: 'Пример заголовка 2',
-        text: 'Текст222222222222'
-    },
-    {
-        title: 'Пример заголовка 3',
-        text: 'Текст33333333'
-    }
-];
-*/
-render(initialNotes);
 
-function openCloseEditor(note = {title: 'Введите заголовок', text: 'Введите текст заметки'}){
-    
-    const editorTitle = document.querySelector('.editor__title');
-    const editorContent = document.querySelector('.editor__content');
-    editorTitle.innerText = note.title;
-    editorContent.innerText = note.text;
-    editor.classList.toggle('editor__show');
-    buttonAdd.classList.toggle('add-close');
-}
-
-function saveNote(notes = [], idx = -1){
-    const editorTitle = document.querySelector('.editor__title');
-    const editorContent = document.querySelector('.editor__content');
-    if(idx >= 0){
-        notes[idx].title = editorTitle.textContent; 
-        notes[idx].text = editorContent.textContent;
-    }else{
+document.querySelector('.add').onclick = () => {
+    openCloseEditor();
+    if(addButtonClose){
+        editorTitle.textContent = '';
+        editorContent.textContent = '';
         notes.push({
-            title: editorTitle.textContent, 
-            text: editorContent.textContent
+            title: '',
+            text: ''
         });
-    }
-    localStorage.setItem('notes', JSON.stringify(notes))
-    render(notes);
-    openCloseEditor();
+        addEventToInputs(notes.length-1);
+    } 
 }
 
-const buttonAdd = document.querySelector('.add');
-const editor = document.querySelector('.editor');
 
 
-buttonAdd.onclick = () => {
+
+function render() {
+    const noteList = document.querySelector('.note-list');
+    noteList.innerHTML = notes.map(function (note, id) {
+        return `
+        <li class="note" >
+        <h2 class="note__title" onclick="editNote(${id})">${note.title}</h2>
+        <p class="note__preview">${note.text}</p>
+        <div class="note__remove" onclick="removeNote(${id})">&times;</div>
+        </li>
+        `
+    }).join('');
+}
+
+
+function removeNote(idx) {
+    notes.splice(idx, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    render();
+}
+
+render();
+
+
+function editNote(idx) {
+    editorTitle.textContent = notes[idx].title;
+    editorContent.textContent = notes[idx].text;
+
     openCloseEditor();
-    const buttonSave = document.querySelector('.editor__save');
-    buttonSave.onclick = () => saveNote(initialNotes);
-};
-//
+    addButtonClose && addEventToInputs(idx);  
+}
+
+function addEventToInputs(idx){
+    editorTitle.dataset.idx = idx;
+    editorContent.dataset.idx = idx;
+// убираем старые слушатели
+    editorTitle.removeEventListener('input', realTimeSaveNote);
+    editorContent.removeEventListener('input', realTimeSaveNote)
+
+    //добавляем новые слушатели
+    editorTitle.addEventListener('input', realTimeSaveNote)
+    editorContent.addEventListener('input', realTimeSaveNote)
+}
 
 
 
 
 
-//noteList.append(listItem);
+function realTimeSaveNote(event) {
+    const index = event.target.dataset.idx;
+
+    clearTimeout(timeOut);
+    timeOut = setTimeout(function(){
+        notes[index].title = editorTitle.textContent;
+        notes[index].text = editorContent.textContent;
+        localStorage.setItem('notes', JSON.stringify(notes))
+        render();
+    }, 500);
+}
+
+
+
+function openCloseEditor() {
+    const editor = document.querySelector('.editor');
+    editor.classList.toggle('editor__show');
+    document.querySelector('.add').classList.toggle('add-close');
+    addButtonClose = !addButtonClose
+}
+
+
